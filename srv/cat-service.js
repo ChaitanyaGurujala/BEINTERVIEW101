8fa8929f6a2e522d1995bb0c45186324f6e6464f
Chaitanya Gurujala <chaitanya.gurujala@gmail.com>
2024-08-08 22:03:14 +0000

    8.8.24 - Afternoon

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,3 +1,5 @@
+const cds = require("@sap/cds/libx/_runtime/cds");
+const { request } = require("express");
 
 class CatalogService extends cds.ApplicationService {
     init() {
@@ -46,10 +48,65 @@ class CatalogService extends cds.ApplicationService {
             return returnData;
 
         });
-        
+
+        this.on("UPDATE", "UpdateEmployeeSrv", async (req, res) => {
+
+            let returnData = await cds.transaction(req).run([
+
+                UPDATE(Employees).set({
+                    nameFirst: req.data.nameFirst
+                }).where({ ID: req.data.ID }),
+
+                UPDATE(Employees).set({
+                    nameLast: req.data.nameLast
+                }).where({ ID: req.data.ID }),
+
+                UPDATE(Employees).set({
+                    phoneNumber: req.data.phoneNumber
+                }).where({ ID: req.data.ID })
+
+
+
+            ]).then((resolve, reject) => {
+                if (typeof (resolve) !== undefined) {
+                    return req.data;
+                } else {
+                    req.error(500, "There was an issue in insert");
+                }
+            }).catch(err => {
+                req.error(500, "there was an error " + err.toString());
+            });
+
+            return returnData;
+
+        });
+
+
+        this.on("DELETE", "DeleteEmployeeSrv", async (req, res) => {
+
+            let returnData = await cds.transaction(req).run([
+
+                DELETE.from(Employees).where(req.data)
+
+            ]).then((resolve, reject) => {
+                if (typeof (resolve) !== undefined) {
+                    return req.data;
+                } else {
+                    req.error(500, "There was an issue in insert");
+                }
+            }).catch(err => {
+                req.error(500, "there was an error " + err.toString());
+            });
+
+            return returnData;
+
+        });
+
+
+
         return super.init();
     }
 
 }
 
-    module.exports = CatalogService;
+module.exports = CatalogService;
